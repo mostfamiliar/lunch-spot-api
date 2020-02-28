b75c8f640546fd4abc49da1c444a1c965bc21151
torrence <tlstratton@gmail.com>
2020-02-28 09:23:02 -0800

    Add test data

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module lunch-spot-api
 go 1.13
 
 require (
-	github.com/gorilla/handlers v1.4.2 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,10 +10,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Resources
+// https://medium.com/@hugo.bjarred/rest-api-with-golang-and-mux-e934f581b8b5
+// https://stackoverflow.com/questions/40985920/making-golang-gorilla-cors-handler-work
+
+type LunchSpot struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Votes       int    `json:"votes"`
+}
+
+var lunchSpots []LunchSpot
+
 func main() {
 	fmt.Println("Start")
 	router := mux.NewRouter()
-	router.HandleFunc("/signin", suggestions).Methods("POST")
+
+	// Create new lunchSpots for testing
+	spot1 := LunchSpot{"1", "Shalom Y'all", "Delicious Israeli food", 3}
+	spot2 := LunchSpot{"1", "Kati Thai", "Vegetarian Thai food", 5}
+	lunchSpots = append(lunchSpots, spot1, spot2)
+
+	router.HandleFunc("/suggestions", suggestions).Methods("POST")
+	router.HandleFunc("/suggestions", getSuggestions).Methods("GET")
 
 	fmt.Println("Listen and Server")
 	corsObj := handlers.AllowedOrigins([]string{"*"})
@@ -21,4 +42,9 @@ func main() {
 
 func suggestions(rw http.ResponseWriter, rq *http.Request) {
 
+}
+
+func getSuggestions(rw http.ResponseWriter, rq *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(lunchSpots)
 }
